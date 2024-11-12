c9a53d01ca0c4362de66e0f47e4ac5033a091a6e
KapilThaman <kapil.thaman.k@gmail.com>
2024-11-11 21:04:41 -0800

    DP Solved improved Time Complexity

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let calculation = 0;
 function fibnocci(n){
     if(n < 2){
         return n
@@ -8,3 +9,27 @@ function fibnocci(n){
 }
 // Optimize it by memoizing the values or caching them using DP
 
+function fibnocciMemoized(){
+    let cache = {};
+    return function fib(n){
+        calculation++;
+        if(n in cache){
+            return cache[n];
+        }
+        else{
+            if(n < 2){
+                return n
+            }
+            else{
+                cache[n] =  fib(n-1) + fib(n-2);
+                return cache[n];
+            }
+        }
+    }
+}
+
+const fasterFib = fibnocciMemoized();
+
+
+console.log(fasterFib(10));
+console.log( "Calculation : " + calculation);
